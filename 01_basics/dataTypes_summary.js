8c7516e71e9e7b80d92d482589c944b4c90e54b5
CaptainLevi101 <93577803+CaptainLevi101@users.noreply.github.com>
2023-09-15 19:14:39 +0000

    dataTypesSummary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,20 @@
+// #primitive data types
+//7 types
+//Number,String,Boolean,Null,Undefined,symbol,BigInt
+//Non Primitive
+//Array,Object,Function
+//javascript is Dynamically typed language
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
