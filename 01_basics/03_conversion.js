70cedac06fdd88f88237372575312aa2ab8204fb
CaptainLevi101 <93577803+CaptainLevi101@users.noreply.github.com>
2023-09-15 18:18:00 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score="Ash"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//number->number is easy
+//string->number is NAN
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+//1->true
+//0->false
+//empty string->false
+//filled string->true
+//can convert number->string as it is
+
