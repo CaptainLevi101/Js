3e68544cb4cb48e2ed674226ba532c404704edc1
CaptainLevi101 <93577803+CaptainLevi101@users.noreply.github.com>
2023-09-17 17:45:53 +0000

    Nums  and Maths

---
+++ 01_basics/04_num.js
@@ -0,0 +1,20 @@
+const score=400
+const balance=new Number(100);
+// console.log(balance);
+// // console.log(score);
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2)); //used to fix the precisioni value
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));  //convert it into a standard form
+        ////++++++++++++++++++++Maths+++++++++++++++++++///
+// console.log(Math); //give the object
+// console.log(Math.abs(-4)); //convert to abs value
+// console.log(Math.round(4.3)); //round of the number
+// console.log(Math.ceil(4.2)); //give top value
+//console.log(Math.min(4,3,6,8))
+//console.log(Math.random()); //give off the random value
+//to get value in range
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
